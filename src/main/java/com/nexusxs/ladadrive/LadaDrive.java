1ee1f6bbca2d43cdd2aaf6ca8b341c68db87ce53
محمد عادل <  GitHub >
2026-09-24 21:01:54 +0300

    Initial LadaDrive project

---
+++ src/main/java/com/nexusxs/ladadrive/LadaDrive.java
@@ -0,0 +1,18 @@
+package com.nexusxs.ladadrive;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LadaDrive implements ModInitializer {
+
+    public static final String MOD_ID = "ladadrive";
+    public static final Logger LOGGER =
+            LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("LadaDrive initialized.");
+        LOGGER.info("Lada 2107 prototype is starting.");
+    }
+}
